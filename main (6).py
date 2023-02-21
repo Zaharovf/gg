3ec35728e84f213d565451c9463aee9552850e6a
Zaharovf <117991204+Zaharovf@users.noreply.github.com>
2023-02-21 10:19:05 +0300

    Add files via upload

---
+++ main (6).py
@@ -0,0 +1,10 @@
+a = int(input("Введите число A: "))
+b = int(input("Введите число B: "))
+if a!=b:
+    print("suuu")
+elif a == b: 
+    print("suuu")
+elif a>b:
+    print("suuu")
+else:
+    print("suuu")
